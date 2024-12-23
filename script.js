29a7d1c8e5cf05661625553200acf2e06469dac7
David Truong <davidtruong063@gmail.com>
2024-12-23 15:37:12 -0800

    Implement functionality to Change Grid Size button

--- script.js
+++ script.js
@@ -2,17 +2,32 @@
 function setupGrid(num)
 {
     const ContainerDiv = document.querySelector("#container");
+    ContainerDiv.innerHTML = '';
     ContainerDiv.style.width = `${Math.sqrt(num) * 52}px`;
     ContainerDiv.style.height = `${Math.sqrt(num) * 52}px`;
     for(let i = 0; i < num; i++)
     {
-        const gridSquare = document.createElement("div");
+        const GridSquare = document.createElement("div");
 
-        gridSquare.className = "square";
-        gridSquare.style.width= "50px";
-        gridSquare.style.height = "50px";
-        ContainerDiv.appendChild(gridSquare);
+        GridSquare.className = "square";
+        GridSquare.style.width= "50px";
+        GridSquare.style.height = "50px";
+        GridSquare.backgroundColor = "light-cyan"
+        ContainerDiv.appendChild(GridSquare);
     }
 }
 
+function turnOn(GridSquare)
+{
+    GridSquare.
+}
+
 setupGrid(64);
+
+const button = document.querySelector("#btn1");
+button.addEventListener("click", () => {
+    const NewGridSize = prompt("Enter the new size of grid!");
+    setupGrid(NewGridSize);
+
+
+});
